da3841b595f4e29c776744eb91056e46c48c550e
Quách Quang Ngọc <qqngoc2988@gmail.com>
2025-05-08 18:47:42 +0700

    Sắp thự tự nhà thầu theo hạng mục

--- framework-customizations/extensions/shortcodes/shortcodes/estimates/views/view.php
+++ framework-customizations/extensions/shortcodes/shortcodes/estimates/views/view.php
@@ -18,25 +18,8 @@ if($contractor_cats && $client) {
 	<?php
 	foreach ($contractor_cats as $key => $value) {
 		if($key>0) {
-			$contractors = get_posts([
-				'post_type' => 'contractor',
-				'posts_per_page' => -1,
-				'post_status' => 'publish',
-				'fields' => 'ids',
-				'tax_query' => [
-					'cat' => [
-						'taxonomy' => 'contractor_cat',
-						'field' => 'id',
-						'terms' => [$value->term_id]
-					],
-					'rating' => [
-						'taxonomy' => 'contractor_rating',
-						'field' => 'id',
-						'terms' => fw_get_db_settings_option('contractor_rating_top')
-					]
-				]
-			]);
-			if($contractors) {
+			$childrent = get_terms(['taxonomy' => 'contractor_cat','parent'=>$value->term_id]);
+			if($childrent) {
 			?>
 			<section class="accordion-item mb-3">
 				<h2 class="accordion-header">
@@ -46,63 +29,85 @@ if($contractor_cats && $client) {
       				<div class="accordion-body">
 						<div class="row">
 						<?php
-						foreach($contractors as $contractor_id) {
-							$estimates = get_post_meta($contractor_id, '_estimates', true);
-							$estimate = isset($estimates[$client->term_id])?$estimates[$client->term_id]:[ 'value'=>'', 'attachment_id'=>''];
-							//debug($estimate);
-							$phone_number = get_post_meta($contractor_id, '_phone_number', true);
-							$external_url = get_post_meta($contractor_id, '_external_url', true);
-							$external_url = ($external_url!='')?esc_url($external_url):'#';
+						foreach ($childrent as $child) {
+							$contractors = get_posts([
+								'post_type' => 'contractor',
+								'posts_per_page' => -1,
+								'post_status' => 'publish',
+								'fields' => 'ids',
+								'tax_query' => [
+									'cat' => [
+										'taxonomy' => 'contractor_cat',
+										'field' => 'id',
+										'terms' => [$child->term_id]
+									],
+									'rating' => [
+										'taxonomy' => 'contractor_rating',
+										'field' => 'id',
+										'terms' => fw_get_db_settings_option('contractor_rating_top')
+									]
+								]
+							]);
+							if($contractors) {
+								foreach($contractors as $contractor_id) {
+									$estimates = get_post_meta($contractor_id, '_estimates', true);
+									$estimate = isset($estimates[$client->term_id])?$estimates[$client->term_id]:[ 'value'=>'', 'attachment_id'=>''];
+									//debug($estimate);
+									$phone_number = get_post_meta($contractor_id, '_phone_number', true);
+									$external_url = get_post_meta($contractor_id, '_external_url', true);
+									$external_url = ($external_url!='')?esc_url($external_url):'#';
 
-							$cats = get_the_terms( $contractor_id, 'contractor_cat' );
-							?>
-							<div class="col-lg-3 col-md-6 estimate-item mb-4">
-								<div class="estimate border border-dark">
-									<div class="contractor-thumbnail position-relative">
-										<a class="thumbnail-image position-absolute w-100 h-100 start-0 top-0" href="<?=$external_url?>" target="_blank"><?php echo get_the_post_thumbnail( $contractor_id, 'full' ); ?></a>
-										<?php if(has_role('administrator')) { ?>
-										<button type="button" class="btn btn-sm btn-danger text-yellow fw-bold m-1 position-absolute bottom-0 end-0" data-bs-toggle="modal" data-bs-target="#edit-estimate" data-client="<?=$client->term_id?>" data-contractor="<?=$contractor_id?>" data-contractor-title="<?php echo esc_attr(get_the_title( $contractor_id )); ?>"><span class="dashicons dashicons-edit"></span></button>
-										<?php } ?>
-									</div>
-									<div class="contractor-info contractor-info-<?=$contractor_id?> text-center px-1">
-										<div class="contractor-title pt-3 mb-1 fs-5">
-											<a class="d-block text-truncate" href="<?=$external_url?>" target="_blank" title="<?php echo esc_attr(get_the_title( $contractor_id )); ?>"><?php echo esc_html(get_the_title( $contractor_id )); ?></a>
-											<div class="text-truncate fs-6 text-yellow d-flex flex-wrap justify-content-center">
-												<?php
-												if($cats) {
-													foreach ($cats as $key => $cat) {
-														echo '<div>'.(($key>0)?', ':' ').esc_html($cat->name).'</div>';
+									$cats = get_the_terms( $contractor_id, 'contractor_cat' );
+									?>
+									<div class="col-lg-3 col-md-6 estimate-item mb-4">
+										<div class="estimate border border-dark">
+											<div class="contractor-thumbnail position-relative">
+												<a class="thumbnail-image position-absolute w-100 h-100 start-0 top-0" href="<?=$external_url?>" target="_blank"><?php echo get_the_post_thumbnail( $contractor_id, 'full' ); ?></a>
+												<?php if(has_role('administrator')) { ?>
+												<button type="button" class="btn btn-sm btn-danger text-yellow fw-bold m-1 position-absolute bottom-0 end-0" data-bs-toggle="modal" data-bs-target="#edit-estimate" data-client="<?=$client->term_id?>" data-contractor="<?=$contractor_id?>" data-contractor-title="<?php echo esc_attr(get_the_title( $contractor_id )); ?>"><span class="dashicons dashicons-edit"></span></button>
+												<?php } ?>
+											</div>
+											<div class="contractor-info contractor-info-<?=$contractor_id?> text-center px-1">
+												<div class="contractor-title pt-3 mb-1 fs-5">
+													<a class="d-block text-truncate" href="<?=$external_url?>" target="_blank" title="<?php echo esc_attr(get_the_title( $contractor_id )); ?>"><?php echo esc_html(get_the_title( $contractor_id )); ?></a>
+													<div class="text-truncate fs-6 text-yellow d-flex flex-wrap justify-content-center">
+														<?php
+														if($cats) {
+															foreach ($cats as $key => $cat) {
+																echo '<div>'.(($key>0)?', ':' ').esc_html($cat->name).'</div>';
+															}
+														}
+														?>
+													</div>
+												</div>
+												
+												<div class="contractor-value mb-1">
+													<span>Tổng giá trị:</span>
+													<span class="text-red fw-bold"><?php echo ($estimate['value']) ? esc_html(number_format($estimate['value'],0,'.',',')) : ''; ?></span>
+												</div>
+												
+												<div class="d-flex flex-wrap justify-content-center contractor-links mb-3">
+													<?php
+													if($phone_number) {
+														?>
+														<a class="btn btn-sm btn-danger my-1 mx-2" href="tel:<?=esc_attr($phone_number)?>"><?=esc_html($phone_number)?></a>
+														<?php
 													}
-												}
-												?>
+
+													if($estimate['attachment_id']) {
+														$attachment_url = wp_get_attachment_url($estimate['attachment_id']);
+														?>
+														<a class="btn btn-sm btn-primary my-1 mx-2" href="<?=esc_url($attachment_url)?>" target="_blank">Xem dự toán</a>
+														<?php
+													}
+													?>
+												</div>
 											</div>
 										</div>
-										
-										<div class="contractor-value mb-1">
-											<span>Tổng giá trị:</span>
-											<span class="text-red fw-bold"><?php echo ($estimate['value']) ? esc_html(number_format($estimate['value'],0,'.',',')) : ''; ?></span>
-										</div>
-										
-										<div class="d-flex flex-wrap justify-content-center contractor-links mb-3">
-											<?php
-											if($phone_number) {
-												?>
-												<a class="btn btn-sm btn-danger my-1 mx-2" href="tel:<?=esc_attr($phone_number)?>"><?=esc_html($phone_number)?></a>
-												<?php
-											}
-
-											if($estimate['attachment_id']) {
-												$attachment_url = wp_get_attachment_url($estimate['attachment_id']);
-												?>
-												<a class="btn btn-sm btn-primary my-1 mx-2" href="<?=esc_url($attachment_url)?>" target="_blank">Xem dự toán</a>
-												<?php
-											}
-											?>
-										</div>
 									</div>
-								</div>
-							</div>
-						<?php } ?>
+							<?php }
+							}
+						} ?>
 						</div>
 					</div>
 				</div>
